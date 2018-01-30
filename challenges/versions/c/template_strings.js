1127c342365d184025c19eb31033a0a554556c47
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-01-29 17:15:09 -0800

    Add strings for version c

--- challenges/versions/c/template_strings.js
+++ challenges/versions/c/template_strings.js
@@ -7,11 +7,16 @@ const stringMaker = function(dbStrings) {
 
   const strings = {}
 
-  strings.dbFuncName = `get${dbStrings.mainTableRootNameCaps}For${dbStrings.secondaryTableRootNameCaps}`
-  strings.parameterName = `${dbStrings.secondaryTableRootNameCaps}Id`
+  strings.dbFuncName = `get${dbStrings.mainTableRootNameCaps}sFor${dbStrings.secondaryTableRootNameCaps}Id`
+  strings.parameterName = `${dbStrings.secondaryTableRootName}Id`
   strings.dataColumnName = dbStrings.mainTableMainColumnName
   strings.dataColumnDescription = `${dbStrings.mainTableRootName} ${dbStrings.mainTableMainColumnName}`
-  strings.sampleDbResult = ``
+  strings.sampleDbResult = dbStrings.exampleResults.mainTableMainColumn
+  strings.endpoint = `/${dbStrings.secondaryTableRootName}s/:${dbStrings.secondaryTableRootName}Id/${dbStrings.mainTableRootName}s`
+  strings.endpointFilesBaseName = `${dbStrings.secondaryTableRootName}_${dbStrings.mainTableRootName}s`
+  strings.endpointExample = `/${dbStrings.secondaryTableRootName}s/3/${dbStrings.mainTableRootName}s`
+  strings.viewTitle = `${dbStrings.secondaryTableRootNameCaps} ${dbStrings.mainTableRootName}s`
+  strings.buttonId = `add-${dbStrings.mainTableRootName}`
 
   return Object.assign(dbStrings, strings)
 
